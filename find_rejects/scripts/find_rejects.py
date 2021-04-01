d8e2cc3acecbe06849d279ebc7ab80503c2d87f1
Xoma <xomyakes@gmail.com>
2021-04-01 10:08:58 +0300

    Console func with test commands added

--- find_rejects/scripts/find_rejects.py
+++ find_rejects/scripts/find_rejects.py
@@ -1,11 +1,25 @@
 import os
-
+import requests
 
 
 def main():
     print('Hello, mothrfucker!')
-
-
+    console()
+def test():
+    print('It works!')
+def console():
+    command = ''
+    while True:
+        command = str(input())
+        try:
+            if command in commands:
+                commands[command]()
+        except requests.exceptions.RequestException:
+            print("Can't read command")
+commands = {
+    'test' : test,
+    'exit' : exit
+}
 
 
 if __name__ == "__main__":
